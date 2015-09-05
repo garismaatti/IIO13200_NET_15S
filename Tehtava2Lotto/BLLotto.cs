3b5fdb9f6d49888383cc90cdf3f779e1bdf38eb3
Ari Salopaa <ari.salopaa@gmail.com>
2015-09-05 22:57:53 +0300

    Working code of T2 with some hidden small features.

--- Tehtava2Lotto/BLLotto.cs
+++ Tehtava2Lotto/BLLotto.cs
@@ -6,16 +6,113 @@ using System.Threading.Tasks;
 
 namespace Tehtävä2___lotto
 {
-    class BLLotto
-    {
-        //List<Part> parts = new List<Part>();
-    }
 
     class Lotto
     {
-        int SuurinNro = 0;
-        int NumeroLkm = 0;
-        string Tyyppi = "";
+        private int SuurinNro = 39;
+        private int PieninNro = 1;
+        private int NumeroLkm = 7;
+        /*  Tyyppi on
+            0 = Lotto
+            1 = Viikinlotto
+            2 = Eurojackpot
+        */
+        private int Tyyppi = 0;
+        private int Riveja = 0;
+        private Random rnd1 = new Random();
+
+
+        public List<string> drawLottery(int game, int rws)
+        {
+            setGameType(game);
+            if (rws > -1) //check validity
+            {
+                Riveja = rws;
+            }
+            List<string> lista = new List<string>();
+            for (int k = 0; k < Riveja; k++)
+            {
+                lista.Add(drawRow());
+            }
+            return lista;
+        }
+
+        private string drawRow()
+        {
+            string str1 = "";
+            for (int i=0; i < NumeroLkm; i++)
+            {
+                int newNum = 0;
+                if (Tyyppi == 2 && i >= 4)//Eurojackpot extra numbers
+                {
+                    newNum = randomNumber(PieninNro, 8);
+                }
+                else
+                {
+                    newNum = randomNumber(PieninNro, SuurinNro);
+                }
+                if (newNum < 10)
+                {
+                    str1 += " ";
+                }
+                str1 += newNum.ToString();
+
+                //separators
+                if (Tyyppi == 2 && i == 4)
+                {
+                    str1 += " -- ";
+                }
+                else
+                {
+                    if (i != NumeroLkm - 1)
+                    {
+                        str1 += ", ";
+                    }
+                }
+                
+                
+            }
+            return str1;
+        }
+
+        //Function to generate random number
+        private int randomNumber(int min, int max)
+        {
+            return rnd1.Next(min,max);
+        }
+
+        //Set game type
+        //Return 1 if there were error, otherwise return 0
+        public int setGameType(int type)
+        {
+            if (type == 1)
+            {
+                Tyyppi = type;
+                SuurinNro = 48;
+                NumeroLkm = 6;
+            }
+            else if (type == 2)
+            {
+                Tyyppi = type;
+                SuurinNro = 50;
+                NumeroLkm = 7;
+            }
+            else if (type == 0)
+            {
+                Tyyppi = type;
+                SuurinNro = 39;
+                NumeroLkm = 7;
+            }
+            else
+            {
+                //There were error, use default values
+                Tyyppi = 0;
+                SuurinNro = 39;
+                NumeroLkm = 7;
+                return 1;
+            }
+            return 0;
+        }
 
 
     }
--- Tehtava2Lotto/MainWindow.xaml.cs
+++ Tehtava2Lotto/MainWindow.xaml.cs
@@ -23,11 +23,63 @@ namespace Tehtava2Lotto
         public MainWindow()
         {
             InitializeComponent();
+            txtRows.Focus();
+            txtRows.SelectAll();
         }
+
+        Tehtävä2___lotto.Lotto newGame = new Tehtävä2___lotto.Lotto();
+        int lastGame = -1;
 
         private void btnDraw_Click(object sender, RoutedEventArgs e)
         {
+            int rws = 0;
+            if (int.TryParse(txtRows.Text, out rws))
+            {
+                if (rws < 0)
+                {
+                    //check validity
+                    txtRows.Background = Brushes.Red;
+                    MessageBox.Show("Rivien lukumäärä ei voi olla negatiivinen!");
+                    txtRows.Background = Brushes.White;
+                }
+                else
+                {
+                    if (rws > 99)
+                    {
+                        MessageBoxResult result = MessageBox.Show("Hupsista! Yrität luoda yli 100 riviä kerralla!\nEmme suosittele luomaan yli 100 riviä kerralla vaan generoi esimerkiksi 10 riviä useampaan kertaan.\n\nToiminnon suorittaminen saattaa kestää pitkään ja aiheuttaa häiriöitä tietokoneen muuhun toimintoihin. Haluatko silti jatkaa?", "Vahvista rivien generointi!", MessageBoxButton.YesNo ,MessageBoxImage.Warning);
 
+                        if (result == MessageBoxResult.No)
+                        {
+                            rws = 0;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //Failed to parse int
+                txtRows.Background = Brushes.Red;
+                MessageBox.Show("Anna kelvollinen rivien lukumäärä!");
+                txtRows.Background = Brushes.White;
+            }
+            if (lastGame != cbxGameSelector.SelectedIndex)
+            {
+                lbxResult.Items.Add("");//spacer
+                lbxResult.Items.Add(cbxGameSelector.Text); //add game name
+            }
+            
+            List<string> lista = newGame.drawLottery(cbxGameSelector.SelectedIndex, rws);
+            for (int j=lista.Count-1; j >= 0; j--)
+            {
+                lbxResult.Items.Add( lista.ElementAt(j) );
+                lbxResult.SelectedIndex = lbxResult.Items.Count - 1;
+                lbxResult.ScrollIntoView(lbxResult.SelectedItem);
+            }
+            //more comfort
+            txtRows.Focus();
+            txtRows.SelectAll();
+            //update last game
+            lastGame = cbxGameSelector.SelectedIndex;
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
@@ -35,20 +87,24 @@ namespace Tehtava2Lotto
             lbxResult.Items.Clear();
         }
 
-        private void cbxGameSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Oletko varma että haluat sulkea ohjelman?\nKaikki tallentamattomat tiedot menetetään!", "Vahvista sulkeminen", MessageBoxButton.YesNo);
+            MessageBoxResult result = MessageBox.Show("Oletko varma että haluat sulkea ohjelman?\nKaikki tallentamattomat tiedot menetetään!", "Vahvista sulkeminen", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
             if (result == MessageBoxResult.Yes)
             {
                 Close();
             }
             
+        }
+
+
+        //just making it more comfortable to use
+        private void cbxGameSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtRows.Focus();
+            txtRows.SelectAll();
         }
     }
 }
