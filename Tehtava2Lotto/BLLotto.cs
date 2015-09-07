a39bb4aa632a9af89b51656fbc65956ed33f3b8b
Ari Salopaa <ari.salopaa@gmail.com>
2015-09-07 19:46:13 +0300

    T2 - Rewrited to use list of int and print numbers in order + some comments

--- Tehtava2Lotto/BLLotto.cs
+++ Tehtava2Lotto/BLLotto.cs
@@ -20,63 +20,121 @@ namespace Tehtävä2___lotto
         private Random rnd1 = new Random();
 
 
+
+        //Start function
         public List<string> drawLottery(string game, int rws)
         {
-            setGameType(game);
+            setGameType(game); //set game type
             if (rws < 0) //check validity
             {
                 rws = 0;
             }
-            List<string> lista = new List<string>();
-            for (int k = 0; k < rws; k++)
+            List<List<int>> lista = new List<List<int>>(); //Numeric list of draw rows
+            List<string> strRowList = new List<string>();    //String list of rows
+
+            for (int k = 0; k < rws; k++) //fill all rows
             {
+                //Draw numbers
                 lista.Add(drawRow());
             }
-            return lista;
+
+            string tmpRowStr = "";   //Hold 1 row temporally
+            List<int> tmpIntList = new List<int>(); //Hold 1 numeric row temporally
+
+            for (int h = 0; h < lista.Count; h++)//for each (row)
+            {
+                tmpRowStr = ""; //clear tmpRowString
+                tmpIntList = lista.ElementAt(h); //Set temporally numeric row
+
+                for (int i = 0; i < tmpIntList.Count; i++) //For each number in row
+                {
+                    int newNum = tmpIntList.ElementAt(i); //get number
+                    if (newNum < 10) //add spacing
+                    {
+                        tmpRowStr += " ";
+                    }
+                    tmpRowStr += newNum.ToString(); //add number
+
+                    //separators
+                    if (Tyyppi == "Eurojackpot" && i == 4)
+                    {
+                        tmpRowStr += " --";
+                    }
+                    else
+                    {
+                        if (i != NumeroLkm - 1)
+                        {
+                            tmpRowStr += ", ";
+                        }
+                    }
+                                        
+                }
+                strRowList.Add(tmpRowStr); //add str to strRowList
+            }
+
+            return strRowList; //return complete list of strings
         }
 
-        private string drawRow()
+
+        private List<int> drawRow()
         {
-            string str1 = "";
-            for (int i=0; i < NumeroLkm; i++)
+            List<int> rivi = new List<int>();
+            for (int i = 0; i < NumeroLkm; i++)
             {
                 int newNum = 0;
-                if (Tyyppi == "Eurojackpot" && i >= 4)//Eurojackpot extra numbers
+                if (Tyyppi == "Eurojackpot" && i > 4)//Eurojackpot extra numbers
                 {
-                    newNum = randomNumber(1, 8);
+                    rivi.Sort(); //sort brevious numbers
+
+                    newNum = randomNumber(1, 10);//first extra number
+                    int secondNum = newNum;
+
+                    while (newNum == secondNum) //prevent same number
+                    {
+                        secondNum = randomNumber(1, 10);//second extra number
+                    }
+
+                    if (newNum > secondNum) //put in numeric order
+                    {
+                        rivi.Add(secondNum);
+                        rivi.Add(newNum);
+                    }
+                    else
+                    {
+                        rivi.Add(newNum);
+                        rivi.Add(secondNum);
+                    }
+                    i = NumeroLkm; //end for loop
+
                 }
                 else
                 {
                     newNum = randomNumber(1, SuurinNro);
-                }
-                if (newNum < 10)
-                {
-                    str1 += " ";
-                }
-                str1 += newNum.ToString();
-
-                //separators
-                if (Tyyppi == "Eurojackpot" && i == 4)
-                {
-                    str1 += " -- ";
-                }
-                else
-                {
-                    if (i != NumeroLkm - 1)
+                    if (rivi.Contains(newNum))
                     {
-                        str1 += ", ";
+                        //already exist
+                        i--;
+                    }
+                    else
+                    {
+                        rivi.Add(newNum);
                     }
                 }
-                
-                
             }
-            return str1;
+            if (Tyyppi != "Eurojackpot"){ //dont sort in here if Euroojackpot
+                rivi.Sort();
+            }
+            
+            return rivi;
         }
+
+
+
 
         //Function to generate random number
         private int randomNumber(int min, int max)
         {
-            return rnd1.Next(min,max);
+            return rnd1.Next(min,max+1);
         }
 
         //Set game type
