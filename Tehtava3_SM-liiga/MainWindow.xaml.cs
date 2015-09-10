da6cca91187338b71dc040d29295b35084447bf3
garismaatti <ari.salopaa@gmail.com>
2015-09-10 16:06:47 +0300

    Tehtävä 3 (T3) melkein valmis, tarkistuksia puuttuu

---
+++ Tehtava3_SM-liiga/MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava3_SM_liiga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddMockupData();    //add mockup data
+            UpdateTeamList();   // update team list
+            UpdateListView();   // update Player list
+            cbTeam.SelectedIndex = 0; //select first item
+        }
+
+
+
+        private List<Seura> NHL = new List<Seura>();
+
+        private void AddMockupData()
+        {
+            string[] seuroja = { "Blues", "HIFK", "HPK", "Ilves", "JYP", "KalPa", "KooKoo", "Kärpät", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Ässät" };
+
+            for (int u = 0; u < seuroja.Length; u++)
+            {
+                Seura seura = new Seura();
+                seura.SeuraNimi = seuroja[u];
+                if (u == 0)
+                {
+                    seura.AddPlayer("Eero", "Somervuori", seura.SeuraNimi, 22000);
+                    seura.AddPlayer("Ilkka", "Kangasniemi", seura.SeuraNimi, 27800);
+                    seura.AddPlayer("Robert", "Rooba", seura.SeuraNimi, 72893);
+                }
+                else if (u == 1)
+                {
+                    seura.AddPlayer("Yohann", "Auvitu", seura.SeuraNimi, 22000);
+                    seura.AddPlayer("Corey", "Elkins", seura.SeuraNimi, 27800);
+                    seura.AddPlayer("Eemeli", "Heikkinen", seura.SeuraNimi, 72893);
+                }
+
+                NHL.Add(seura);
+            }
+            lbStatusBar.Content = "Testi data kirjoitettu tietokantaan!";
+        }
+
+        public int GetSelectedTeam()
+        {
+            int i = cbTeam.SelectedIndex;
+            if (i < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        public int GetSelectedPlayer()
+        {
+            int i = lstPelaajat.SelectedIndex;
+            if (i < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        public void UpdateListView()    //Update list object in GUI
+        {
+            List<Pelaaja> players = new List<Pelaaja>();
+            Pelaaja player = new Pelaaja();
+            Seura seura = new Seura();
+            //cbTeam.Items.Clear();   //clear list
+            lstPelaajat.Items.Clear();
+
+            int i = GetSelectedTeam();
+            //for (int i=0; i < NHL.Count(); i++) //For every Seura
+            // {
+            seura = NHL.ElementAt(i);
+            //cbTeam.Items.Add(seura.SeuraNimi);  //Add Seura
+
+            players = seura.GetPlayers();
+            for (int j = 0; j < players.Count(); j++) //For every player in players list in Seura
+            {
+                player = players.ElementAt(j);
+                lstPelaajat.Items.Add(player.KokoNimi); //Add player to list
+            }
+            // }
+
+        }
+
+
+        public void UpdateTeamList()    //Update team list in GUI
+        {
+            Seura seura = new Seura();
+            cbTeam.Items.Clear();   //clear list
+
+            //int i = GetSelectedTeam();
+            for (int i = 0; i < NHL.Count(); i++) //For every Seura
+            {
+                seura = NHL.ElementAt(i);
+                cbTeam.Items.Add(seura.SeuraNimi);  //Add Seura
+            }
+
+
+
+
+
+        }
+
+        public void UpdatePlayerInfo()
+        {
+            int i = GetSelectedPlayer();
+            Pelaaja a = NHL.ElementAt(GetSelectedTeam()).GetPlayers().ElementAt(i);
+            txtFirstName.Text = a.Etunimi;
+            txtLastName.Text = a.Sukunimi;
+            txtTransferPrice.Text = a.Siirtohinta.ToString();
+        }
+
+
+
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void cbTeam_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lbStatusBar.Content = "Seura vaihtui '" + NHL.ElementAt(GetSelectedTeam()).SeuraNimi + "' ";
+            UpdateListView();
+            UpdatePlayerInfo();
+        }
+
+        private void lstPelaajat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdatePlayerInfo();
+        }
+
+        private void btnAddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+
+            Seura seura = new Seura();
+            seura = NHL.ElementAt(GetSelectedTeam());
+            string f = txtFirstName.Text;
+            string l = txtLastName.Text;
+            float p = 0;
+            if (float.TryParse(txtTransferPrice.Text, out p))
+            {
+                // is value
+                txtTransferPrice.Background = Brushes.White;
+            }
+            else
+            {
+                p = 0;
+            }
+            seura.AddPlayer(f, l, seura.SeuraNimi, p);
+
+            lbStatusBar.Content = "Pelaaja '" +f +" " +l +", " +seura.SeuraNimi +"' lisätty!";
+            UpdateListView();
+        }
+
+        private void btnRemovePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            Seura seura = new Seura();
+            seura = NHL.ElementAt(GetSelectedTeam());
+            string name = seura.GetPlayers().ElementAt(GetSelectedPlayer()).KokoNimi;
+            lbStatusBar.Content = "Pelaaja '" +name +"' poistettu!";
+            seura.RemovePlayer(GetSelectedPlayer());
+            UpdateListView();
+        }
+
+        private void btnSavePlayer_Click(object sender, RoutedEventArgs e)
+        {
+
+            Seura seura = new Seura();
+            seura = NHL.ElementAt(GetSelectedTeam());
+
+            string f = txtFirstName.Text;
+            string l = txtLastName.Text;
+            float p = 0;
+            if (float.TryParse(txtTransferPrice.Text, out p))
+            {
+                // is value
+                txtTransferPrice.Background = Brushes.White;
+            }
+            else
+            {
+                p = 0;
+            }
+            seura.UpdatePlayer(GetSelectedPlayer(), f, l, seura.SeuraNimi, p);
+
+            lbStatusBar.Content = "Pelaaja '" + f + " " + l + ", " + seura.SeuraNimi + "' päivitetty!";
+            UpdateListView();
+        }
+    }
+}
---
+++ Tehtava3_SM-liiga/Pelaaja.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava3_SM_liiga
+{
+    /*Tee ensimmäiseksi luokka Pelaaja, jolla on ominaisuudet 
+    Etunimi, 
+    Sukunimi, 
+    KokoNimi, 
+    Seura, 
+    Siirtohinta. 
+    
+    KokoNimi on read-only tyyppinen ominaisuus, joka muodostetaan etunimi+sukunimi+,seura  siis esim: "Raimo Helminen, Ilves". 
+    Suunnittele ja toteuta luokalle tarvittavat konstruktorit ja metodit. 
+    */
+    class Pelaaja
+    {
+        private string firstName_my = "";
+        private string lastName_my = "";
+        private string team_my = "";
+        private float transferPrice_my = 0;
+
+
+        public string Etunimi
+        {
+            get
+            {
+                return this.firstName_my;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.firstName_my = value;
+                }
+
+            }
+        }
+
+        public string Sukunimi
+        {
+            get
+            {
+                return this.lastName_my;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.lastName_my = value;
+                }
+
+            }
+        }
+
+
+        public string KokoNimi
+        {
+            get
+            {
+                return this.firstName_my + " " + this.lastName_my + ", " + this.team_my;
+            }
+        }
+
+        public string Seura
+        {
+            get
+            {
+                return this.team_my;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.team_my = value;
+                }
+
+            }
+        }
+
+        public float Siirtohinta
+        {
+            get
+            {
+                return this.transferPrice_my;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.transferPrice_my = value;
+                }
+
+            }
+        }
+    }
+
+
+    class Seura
+    {
+        private List<Pelaaja> team = new List<Pelaaja>();
+        private string teamName_my = "";
+        public string SeuraNimi
+        {
+            get
+            {
+                return this.teamName_my;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.teamName_my = value;
+                }
+
+            }
+        }
+
+
+        public void AddPlayer(string n_firstName, string n_lastName, string n_team, float n_price)
+        {
+            Pelaaja n_player = new Pelaaja();
+
+            n_player.Etunimi = n_firstName;
+            n_player.Sukunimi = n_lastName;
+            n_player.Seura = n_team;
+            n_player.Siirtohinta = n_price;
+            team.Add(n_player);
+        }
+
+        public void RemovePlayer(int index)
+        {
+            team.RemoveAt(index);
+        }
+
+        public void UpdatePlayer(int index, string n_firstName, string n_lastName, string n_team, float n_price)
+        {
+            team.ElementAt(index).Etunimi = n_firstName;
+            team.ElementAt(index).Sukunimi = n_lastName;
+            team.ElementAt(index).Seura = n_team;
+            team.ElementAt(index).Siirtohinta = n_price;
+        }
+
+        public List<Pelaaja> GetPlayers()
+        {
+            return team;
+        }
+
+    }
+}
