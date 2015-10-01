a456678b67c15a192eb4a3a23dfb5af188e20193
garismaatti <ari.salopaa@gmail.com>
2015-10-01 12:28:53 +0300

    Tehtävä 6 pohja

---
+++ IIO13200-ADONET-DEMO/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿//Koodannut ja testannut toimivaksi 6.3.2014 EsaSalmik
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.ICT.ADOBlanco
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      IniMyStuff();
+    }
+
+    private void IniMyStuff()
+    {
+      //TODO täytetään combobox asiakkaitten maitten nimillä
+      //esimerkki kuinka App.Configissa oleva connectionstring luetaan
+      lbMessages.Content = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+    }
+
+    private void btnGet3_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+    }
+
+    private void btnGetAll_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+    }
+
+    private void btnGetFrom_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+    }
+
+        private void btnYield_Click(object sender, RoutedEventArgs e)
+        {
+            JAMK.ICT.JSON.JSONPlacebo2015 roskaa = new JAMK.ICT.JSON.JSONPlacebo2015();
+            MessageBox.Show(roskaa.Yield());
+        }
+    }
+}
