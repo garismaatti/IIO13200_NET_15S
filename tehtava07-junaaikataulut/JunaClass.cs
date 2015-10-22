c55f8bb56c83ef406a2f6cea5e3e8b4a24eac09f
garismaatti <ari.salopaa@gmail.com>
2015-10-22 15:55:03 +0300

    teht 7 aloitettu

---
+++ tehtava07-junaaikataulut/JunaClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace tehtava07_junaaikataulut
+{
+    class JunaClass
+    {
+        // {"passengerTraffic":true,"type":"STATION","stationName":"Jyväskylä","stationShortCode":"JY","stationUICCode":240,"countryCode":"FI","longitude":25.75498431951706,"latitude":62.24145541884135}
+        // {"stationShortCode":"ML","stationUICCode":17,"countryCode":"FI","type":"DEPARTURE","trainStopping":false,"cancelled":false,"scheduledTime":"2015-10-22T04:17:00.000Z","actualTime":"2015-10-22T04:23:57.000Z","differenceInMinutes":7},
+        //HUOM jos attribuutin ja propertyn nimet ovat eir niin käytä seuraavaa
+        [JsonProperty("trainNumber")]
+        public int trainNum { get; set; }
+        [JsonProperty("operatorUICCode")]
+        public string opUICCode { get; set; }
+        [JsonProperty("operatorShortCode")]
+        public string opShortCode { get; set; }
+        [JsonProperty("trainType")]
+        public string Type { get; set; }
+        [JsonProperty("trainCategory")]
+        public string Category { get; set; }
+        [JsonProperty("runningCurrently")]
+        public bool running { get; set; }
+        public string departureDate { get; set; }
+
+
+
+
+
+
+/*
+
+        public string stationShortCode { get; set; }
+        public int stationUICCode { get; set; }
+        public string countryCode { get; set; }
+        public string type { get; set; }
+        public bool trainStopping { get; set; }
+        public bool cancelled { get; set; }
+        public string scheduledTime { get; set; }
+        public string actualTime { get; set; }
+        public int differenceInMinutes { get; set; }
+        */
+    }
+
+}
---
+++ tehtava07-junaaikataulut/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json; //for json parser
+
+
+
+namespace tehtava07_junaaikataulut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string json = "";
+
+        private string GetSimpleJsonFromWeb()
+        {
+            try
+            {
+                //string url = string.Format("http://rata.digitraffic.fi/api/v1/live-trains?station=JY");
+                string url = string.Format("http://student.labranet.jamk.fi/~K2362/dotNET/live-trains.json");
+
+                //string url = string.Format(txtJson.Text);
+                //when using "using" we do not leave webclient in memory
+                using (WebClient wc = new WebClient())
+                {
+                    var json = wc.DownloadString(url);
+                    //lbError.Content = "Complete!";
+
+                    return json;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return "{ERROR}";
+            }
+        }
+
+
+        private void Demo2()
+        {
+            //haetaan json
+            json = GetSimpleJsonFromWeb();
+            // muunnetaan olioksi
+            List<JunaClass> junat = JsonConvert.DeserializeObject<List<JunaClass>>(json);
+            //Näytetään UI:ssa
+            //txtJson.Text = json;
+            dgData.DataContext = junat;
+        }
+
+        private void btnGetTimes_Click(object sender, RoutedEventArgs e)
+        {
+            Demo2();
+        }
+    }
+}
